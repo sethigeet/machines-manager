8d8c4ef56c8a87a7d98786e4d92e8d291cfd5ce4
Geet Sethi <geetsethi05@gmail.com>
2022-09-13 19:44:34 +0530

    fix: hide console windows on windows

--- src-tauri/src/shell.rs
+++ src-tauri/src/shell.rs
@@ -2,8 +2,17 @@ use std::process::Command;
 
 use crate::{config::Machine, error::AppError};
 
+#[cfg(windows)]
+fn set_windows_creation_flags(cmd: &mut Command) {
+    use std::os::windows::process::CommandExt;
+    cmd.creation_flags(0x08000000); // CREATE_NO_WINDOW
+}
+
 pub fn get_status_cmd(ip: String) -> Command {
     let mut cmd = Command::new("ping");
+
+    #[cfg(windows)]
+    set_windows_creation_flags(&mut cmd);
 
     if cfg!(windows) {
         // windows version of `ping` uses `-n` for number of requests
@@ -25,6 +34,10 @@ pub fn get_shutdown_cmd(machine: &Machine, password: String) -> Result<Command, 
     }
 
     let mut cmd = Command::new("powershell");
+
+    #[cfg(windows)]
+    set_windows_creation_flags(&mut cmd);
+
     cmd.arg("-c");
     cmd.arg(format!(r#"Import-Module RunAs; $pass = $(ConvertTo-SecureString '{}' -AsPlainText -Force); $credential = New-Object System.Management.Automation.PsCredential "{}\{}",$pass; RunAs -netOnly -user $credential -program "shutdown" -arguments "/s /t 0 /m {}""#, password, machine.ip, machine.user, machine.ip));
 
